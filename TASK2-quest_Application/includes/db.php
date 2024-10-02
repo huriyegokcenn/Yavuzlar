1c5379dd34da86ce7867838d151363103a589dc7
Huriye Gökçen <120250427+huriyegokcenn@users.noreply.github.com>
2024-10-03 01:56:57 +0300

    Delete TASK2-quest_Application directory

--- TASK2-quest_Application/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("sqlite:".__DIR__."/../db/database.sqlite");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Veritabanına bağlanılamadı: " . $e->getMessage());
-}
-?>
--- TASK2-quest_Application/student/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../includes/auth.php';
-requireLogin();
-
-echo "Hoş geldin, öğrenci!";
-?>
-
-<a href="solve_question.php">Soru Çöz</a>
-<a href="../scoreboard.php">Puan Tablosu</a>
